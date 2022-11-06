3513977027f18934d8714cbd4a78a6833089a7a1
Aqeel Abbas <aqeeshah273@gmail.com>
2022-11-06 23:13:40 +0500

    All charts done except consistency

--- resources/views/dashboards/users/layouts/user-dash-layout.blade.php
+++ resources/views/dashboards/users/layouts/user-dash-layout.blade.php
@@ -47,7 +47,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ \URL::to('/home')}}" class="brand-link">
+    {{-- <a href="{{ \URL::to('/home')}}" class="brand-link"> --}}
+    <a href="{{ route('user.dashboard')}}" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">News Analyser</span>
     </a>
